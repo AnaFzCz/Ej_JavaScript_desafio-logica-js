9196df4ae2484317aba44bdb405bbca960a3eb6e
Ana Fernandez <anafzcz@gmail.com>
2025-02-02 15:41:40 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,275 @@
+
+/* DESAFIO 1*/
+/*
+alert('Bienvenidos a nuestro sitio!');
+let nome = "Lua";
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponible = 1000;
+
+
+alert('Error! Rellene todos los campos');
+
+let mensagemDeErro = alert('Error! Rellene todos los campos');
+let getnome = prompt('Cuál es su nombre?');
+let getidade = prompt('Cuál es su edad?');
+
+
+if (getidade >= 18) {
+    console.log('!Puede obtener la calificación!!! Lo lograste');
+} else {
+    alert('Tienes que esperar, lo siento');
+}
+*/
+
+
+//  DESAFIO 2 
+
+// 1.- Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
+
+// alert('Bienvenido a nuestra página');
+
+// let nome = prompt('Cuál es tú nombre?');
+// let diaDeLaSemana = prompt(`Qué dia de la semana es hoy ${nome}?`);
+// let diaDeLaSemanaMin = diaDeLaSemana.toLowerCase();
+
+// if (diaDeLaSemanaMin == "sabado" || diaDeLaSemanaMin == "domingo") {
+//     alert(`Buen fin de semana ${nome}`);
+// } else {
+//     if (diaDeLaSemanaMin == "lunes" || diaDeLaSemanaMin == "martes" || diaDeLaSemanaMin == "miercoles" || diaDeLaSemanaMin == "jueves" || diaDeLaSemanaMin == "viernes") {
+//         alert(`Buena semana ${nome}`);
+//     }
+
+// }
+
+
+// 2.-Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.
+
+// alert('Bienvenid@ Vamos a verificar algunos Positivos y Negativos !!!');
+// let nome = prompt('Cuál es tú nombre?');
+// let firstNum = prompt(`Ingresa el número ${nome}`);
+
+// if (firstNum >= 0) {
+//     alert(`${nome}, es POSITIVO!`);
+// } else {
+//     alert(`${nome}, es NEGATIVO!`);
+// }  
+
+
+
+
+// Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!". Caso contrário, mostre "Tente novamente para ganhar.".
+
+
+// alert('Bienvenid@ al Sistema de Puntuación!!!');
+// let nome = prompt('Cuál es tú nombre?');
+// let firstNum = prompt(`Ingresa la puntuación ${nome}`);
+
+// if (firstNum >= 100) {
+//     alert(`Parabéns, você venceu ${nome}!`);
+// } else {
+//     alert(`Tente novamente para ganhar ${nome}`);
+// }
+
+
+// Crie uma mensagem que informa o usuário sobre o saldo da conta, usando uma template string para incluir o valor do saldo.
+
+// alert('Bienvenidos a nuestro sitio!');
+// let nome = "Ana";
+// let numeroDeVendas = 50;
+// let saldoDisponible = 1000;
+
+// let mensaje = alert(`${nome} tu saldo es de ${saldoDisponible}`);
+
+
+
+
+// Peça ao usuário para inserir seu nome usando prompt. Em seguida, mostre um alerta de boas-vindas usando esse nome. 
+
+// let nome = prompt("Digite su nombre ");
+// alert(`Bienvenid@ ${nome}`);
+
+
+// DESAFIO 3
+
+// 1. Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
+
+// alert("Bienvenido al Desafio 3");
+// let contmax = 10;
+// let contador = 0;
+
+// while (contador < contmax) {
+//     alert(`SUMAMOS 1 Y TENEMOS ${contador + 1}`);
+//     contador++;
+
+// }
+
+
+// 2. Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
+
+// alert("Bienvenido al Desafio 3");
+// let contmax = 10;
+// let contador = 0;
+
+// while (contmax >= 0) {
+//     alert(`RESTAMOS 1 Y TENEMOS ${contmax}`);
+//     contmax = contmax - 1;
+
+// }
+
+
+// 3. Crie um programa de contagem regressiva. Peça um número e conte deste número até 0, usando um loop while no console do navegador.
+
+
+// alert("Bienvenido al Desafio 3 Cuenta Regresiva");
+// let nome = prompt('Cuál es tú nombre?');
+// let contmax = prompt(`Ingresa el número ${nome}`);
+
+// while (contmax >= 0) {
+//     alert(`RESTAMOS 1 Y TENEMOS ${contmax}`);
+//     contmax = contmax - 1;
+
+// }
+
+// 4. Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número, usando um loop while no console do navegador.
+
+// alert("Bienvenido al Desafio 3 Cuenta Progresiva");
+// let nome = prompt('Cuál es tú nombre?');
+// let contmax = prompt(`Ingresa el número ${nome}`);
+// let contador = 0;
+
+// while (contador < contmax) {
+//     alert(`SUMAMOS 1 Y TENEMOS ${contador + 1}`);
+//     contador = contador + 1;
+
+// }
+
+// DESAFIO 4
+// Crie um programa que utilize o console.log para exibir uma mensagem de boas-vindas.
+
+
+// let saludo = "BEINVENIDO AL DESAFIO 4";
+// console.log(saludo);
+
+// // Crie uma variável chamada "nome" e atribua a ela o seu nome. Em seguida, utilize o console.log para exibir a mensagem "Olá, [seu nome]!" no console do navegador.
+
+// let nome = "Ana";
+// console.log(`Olá, ${nome}!`);
+
+// Crie uma variável chamada "nome" e atribua a ela o seu nome. Em seguida, utilize o alert para exibir a mensagem "Olá, [seu nome]!" .
+
+// let nome="Ana";
+// alert(`Olá, ${nome}`);
+
+
+// Utilize o prompt e faça a seguinte pergunta: Qual a linguagem de programação que você mais gosta?. Em seguida, armazene a resposta em uma variável e mostre no console do navegador.
+// alert("Bienvenido al Desafio 4");
+// let nome = "Ana";
+// let pregunta = prompt(`Qual a linguagem de programação que você mais gosta ${nome}?`);
+// console.log(pregunta);
+
+
+// Crie uma variável chamada "valor1" e outra chamada "valor2", atribuindo a elas valores numéricos de sua escolha. Em seguida, realize a soma desses dois valores e armazene o resultado em uma terceira variável chamada "resultado". Utilize o console.log para mostrar a mensagem "A soma de [valor1] e [valor2] é igual a [resultado]." no console.
+
+// alert("Bienvenido al Desafio 4");
+// let nome = "Ana";
+// let valor1 = 4;
+// let valor2 = 8;
+// let resultado = valor1 + valor2;
+// console.log(` ${nome} la suma de ${valor1} + ${valor2} es ${resultado}`
+// );
+
+// Crie uma variável chamada "valor1" e outra chamada "valor2", atribuindo a elas valores numéricos de sua escolha. Em seguida, realize a subtração desses dois valores e armazene o resultado em uma terceira variável chamada "resultado". Utilize o console.log para mostrar a mensagem "A diferença entre [valor1] e [valor2] é igual a [resultado]." no console.
+
+
+// alert("Bienvenido al Desafio 4");
+// let nome = "Ana";
+// let valor1 = 4;
+// let valor2 = 8;
+// let resultado = valor1 + valor2;
+// console.log(`${nome} la resta de ${valor1} -  ${valor2} es ${resultado}`
+// );
+
+
+
+// Peça ao usuário para inserir sua idade com prompt. Com base na idade inserida, utilize um if para verificar se a pessoa é maior ou menor de idade, 
+// exibindo uma mensagem apropriada no console.
+
+// alert("Bienvenido al Desafio 4");
+// let nome = prompt(`Cuál es tu nombre?`);
+// let idade = prompt(`${nome}, cuál es tu edad?`);
+
+// if (idade >= 18) {
+//     console.log(`Genial ${nome}, mayor de 18 años`);
+// } else {
+//     console.log(`Genial ${nome}, menor de 18 años`);
+// }
+
+
+
+
+// Crie uma variável "numero" e peça um valor com prompt verifique se é positivo, negativo ou zero. Use if-else para imprimir a respectiva mensagem.
+
+// alert("Bienvenido al Desafio 4");
+// let nome = prompt(`Cuál es tu nombre?`);
+// let numero = prompt(`${nome}, elige un número?`);
+
+
+// if (numero > !0) {
+//     console.log(`Genial ${nome}, el número es POSITIVO`);
+// } else {
+//     if (numero == 0) {
+//         console.log(`Genial ${nome}, el número es igual a 0`);
+//     } else
+//         if (numero < !0) {
+//             console.log(`Genial ${nome}, el número es NEGATIVO`);
+//         }
+// }
+
+
+// Use um loop while para imprimir os números de 1 a 10 no console.
+
+// alert("Bienvenido al Desafio 4");
+
+// let contador = 1;
+
+// while (contador <= 10) {
+//     console.log(`SUMAMOS 1 Y TENEMOS ${contador}`);
+//     contador = contador + 1;
+
+// }
+
+
+// Crie uma variável "nota" e atribua um valor numérico a ela. Use if-else para determinar se a nota é maior ou igual a 7 e exiba "Aprovado" ou "Reprovado" no console.
+
+// alert("Bienvenido al Desafio 4");
+// let nome = prompt(`Cuál es tu nombre?`);
+// let nota = 8;
+// if (nota >= 7) {
+//     console.log(`${nome} = APROBADO`);
+// } else {
+//     console.log(`${nome}  = REPROBADO`)
+// }
+
+
+// Use o Math.random para gerar qualquer número aleatório e exiba esse número no console.
+
+// alert("Bienvenido al Desafio 4");
+// let nome = prompt(`Cuál es tu nombre?`);
+// let numAleatorio = Math.random() * 100 + 1;
+// console.log(`${nome}, este es tu número aleatorio ${numAleatorio}`);
+
+
+// Use o Math.random para gerar um número inteiro entre 1 e 10 e exiba esse número no console.
+// alert("Bienvenido al Desafio 4");
+// let nome = prompt(`Cuál es tu nombre?`);
+// let numAleatorio = parseInt(Math.random() * 10 + 1);
+// console.log(`${nome}, este es tu número aleatorio ${numAleatorio}`);
+
+
+// Use o Math.random para gerar um número inteiro entre 1 e 1000 e exiba esse número no console.
+
+alert("Bienvenido al Desafio 4");
+let nome = prompt(`Cuál es tu nombre?`);
+let numAleatorio = parseInt(Math.random() * 1000 + 1);
+console.log(`${nome}, este es tu número aleatorio ${numAleatorio}`);
